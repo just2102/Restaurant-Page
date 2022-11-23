044b53a6c8d3d4d12314f902cc59e67f1244b514
just2102 <thesixteensaltines@gmail.com>
2022-11-23 09:49:01 +0900

    header hardcoded

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+import { pageLoad } from "./pageLoad"
+
+pageLoad()
---
+++ src/pageLoad.js
@@ -0,0 +1,12 @@
+function pageLoad() {
+    let content = document.getElementById('content');
+    
+    // HEADER AND ITS CONTENTS
+    let header = document.getElementsByTagName('header')
+    
+}
+
+
+
+
+export {pageLoad}
